56f196a60500d6a12067c0aa4088a09140404e6f
Fesuy <fesuybiz@gmail.com>
2023-10-30 20:26:37 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import net from 'net';
+import { EventEmitter } from 'events';
 import pipe from './pipe.js';
 
 const DEFAULT_OPTIONS = {
@@ -10,18 +12,18 @@ const DEFAULT_OPTIONS = {
  * Aulia IPC Class
  * @class
  */
-class Aulia {
+class Aulia extends EventEmitter {
 
   /**
    * Constructor
    * @param options
    */
   constructor(options) {
+    super();
     this.options      = Object.assign(DEFAULT_OPTIONS, options);
     this.options.path = pipe(this.options.path);
 
-    // Nodemon breaking the exists socket path
-    // so annoying
+    // Nodemon breaking when socket path exists
     this.cleanUp_();
   }
 
@@ -31,6 +33,23 @@ class Aulia {
   cleanUp_() {
     if (fs.existsSync(this.options.path)) {
       fs.unlinkSync(this.options.path);
+    }
+  }
+
+  /**
+   * Run IPC Server
+   */
+  run() {
+
+    const server = net.createServer();
+
+    server.on('error', err => this.emit('error', err));
+    server.on('listening', () => this.emit('ready'));
+
+    try {
+      server.listen({ path: this.options.path });
+    } catch (err) {
+      this.emit('error', err);
     }
   }
 }
